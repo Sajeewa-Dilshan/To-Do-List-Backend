6e18137342d9aba04a66d97d03708dae0cc8ed09
Sajeewa Dilshan <sajeewadilshansl@gmail.com>
2021-01-27 16:00:57 +0530

    Add ContextListener and modify UserServlet

--- src/main/java/lk/ijse/dep/web/api/UserServlet.java
+++ src/main/java/lk/ijse/dep/web/api/UserServlet.java
@@ -1,4 +1,30 @@
 package lk.ijse.dep.web.api;
 
-public class UserServlet {
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet
+public class UserServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String query =req.getParameter("q");
+        if(query !=null){
+
+        }
+
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
 }
