cfa2660e19b6080d1ee4d84421d3a4b6e9c6c479
Sajeewa Dilshan <sajeewadilshansl@gmail.com>
2021-01-27 23:35:44 +0530

    Modify ContextListener

--- src/main/java/lk/ijse/dep/web/listener/ContextListener.java
+++ src/main/java/lk/ijse/dep/web/listener/ContextListener.java
@@ -1,10 +1,13 @@
 package lk.ijse.dep.web.listener;
 
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Properties;
 
 @WebListener
@@ -18,11 +21,14 @@ public class ContextListener implements ServletContextListener {
         try{
 
             properties.load(this.getClass().getResourceAsStream("/application.properties"));
-
-
-
-
-
+            BasicDataSource bds= new BasicDataSource();
+            bds.setUsername(properties.getProperty("mysql.username"));
+            bds.setPassword(properties.getProperty("mysql.password"));
+            bds.setUrl(properties.getProperty("mysql.url"));
+            bds.setDriverClassName(properties.getProperty("mysql.driver_class"));
+            bds.setInitialSize(5);
+            bds.setMaxTotal(10);
+            sce.getServletContext().setAttribute("cp",bds);
 
         } catch (IOException exception) {
             exception.printStackTrace();
@@ -32,6 +38,17 @@ public class ContextListener implements ServletContextListener {
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
+        BasicDataSource bds = (BasicDataSource) sce.getServletContext().getAttribute("cp");
+
+        try{
+            bds.close();
+            System.out.println("Closing connection pool..!");
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
 
     }
 }
