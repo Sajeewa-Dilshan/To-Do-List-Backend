a3a8d983533125d518e7ebec2170de1b2ff2de3d
Sajeewa Dilshan <sajeewadilshansl@gmail.com>
2021-01-27 22:33:01 +0530

    Modify the build.gradle

--- src/main/java/lk/ijse/dep/web/listener/ContextListener.java
+++ src/main/java/lk/ijse/dep/web/listener/ContextListener.java
@@ -4,6 +4,8 @@ package lk.ijse.dep.web.listener;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import java.io.IOException;
+import java.util.Properties;
 
 @WebListener
 public class ContextListener implements ServletContextListener {
@@ -11,6 +13,20 @@ public class ContextListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("Initializing connection pool");
+        Properties properties= new Properties();
+        try{
+
+            properties.load(this.getClass().getResourceAsStream("/application.properties"));
+
+
+
+
+
+
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
 
     }
 
